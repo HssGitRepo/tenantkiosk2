edd04d3873caac88cafb4ee85a76cbbfd24c95ed
wasim <wasimshariff001@gmail.com>
2015-11-01 19:01:20 +0530

    Initial commit

---
+++ app/src/main/java/com/ownerkiosk/sample/tenantkiosk2/MainActivity.java
@@ -0,0 +1,77 @@
+package com.ownerkiosk.sample.tenantkiosk2;
+
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
+public class MainActivity extends ActionBarActivity {
+    TextToSpeech tts;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((Button)findViewById(R.id.scan)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+    private void speakOut(final String text) {
+
+        tts = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    int result = tts.setLanguage(Locale.getDefault());
+//                    tts.setPitch(1.0f);int result = tts.setLanguage(Locale.US);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(getApplicationContext(), "Voice not supported", Toast.LENGTH_LONG).show();
+                    } else {
+                        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+                    }
+                }
+
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+//                tvStatus.setText(intent.getStringExtra("SCAN_RESULT_FORMAT"));
+//                Toast.makeText(getApplicationContext(), "" + intent.getStringExtra("SCAN_RESULT"), Toast.LENGTH_SHORT).show();
+
+                speakOut(""+intent.getStringExtra("SCAN_RESULT"));
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+}
